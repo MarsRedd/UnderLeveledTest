3fb493b1f75c94795ca98de76a51784bc17edcbf
a1clark1a <a1clark1a@gmail.com>
2018-08-13 00:55:47 -0700

    Clarks UE_LOG test

--- Undr_Lvled_TEst/Source/Undr_Lvled_TEst/Private/ClarkChairReporter.cpp
+++ Undr_Lvled_TEst/Source/Undr_Lvled_TEst/Private/ClarkChairReporter.cpp
@@ -1,32 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Position_Reporter.h"
-#include "Undr_Lvled_TEst.h"
+#include "ClarkChairReporter.h"
+#include "GameFramework/Actor.h"
+
 
 
 // Sets default values for this component's properties
-UPosition_Reporter::UPosition_Reporter()
+UClarkChairReporter::UClarkChairReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	UE_LOG(LogTemp, Warning, TEXT("Dom's Chair Here and Accounted For!"));
 
+	// ...
 }
+
 
 // Called when the game starts
-void UPosition_Reporter::BeginPlay()
+void UClarkChairReporter::BeginPlay()
 {
 	Super::BeginPlay();
-
+	FString ChairName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s is reporting for booty!!"), *ChairName);
+	// ...
 	
 }
-	
-
 
 
 // Called every frame
-void UPosition_Reporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UClarkChairReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
